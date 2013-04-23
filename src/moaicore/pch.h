f210439994b0efce2caf22c5f6b9f2913ef1e255
Patrick Meehan <patrick@ziplinegames.com>
2013-04-23 12:12:27 -0700

    fixed straggling zl_gfx_opengl bug

--- src/moaicore/pch.h
+++ src/moaicore/pch.h
@@ -1,16 +1,2 @@
 #include <uslscore/uslscore.h>
-
-#ifdef __FLASCC__
-
-	//#include <AS3/AS3.h>
-	//#include <Flash++.h>
-	//#include <AGAL.h>
-
-	//#include <GL/gl.h>
-	//#include <GL/glu.h>
-	//#include <GL/glext.h>
-#endif
-
-#ifdef _ARM_ARCH_7
-	#define MOAI_ARM7
-#endif
+#include <moaicore-config.h>
